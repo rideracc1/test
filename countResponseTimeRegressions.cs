368ed13616b9163fd52ad4acfb0f90b0e33236c0
Correct Name <rideracc1@proton.me>
2026-06-21 03:17:30 +0400

    Count Elements Greater Than Previous Average

---
+++ countResponseTimeRegressions.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine(countResponseTimeRegressions([100, 200, 150,300]));
+
+static int countResponseTimeRegressions(List<int> responseTimes)
+{
+    if (responseTimes.Count < 2)
+    {
+        return 0;
+    }
+
+    int result = 0;
+    for (int i = 1; i < responseTimes.Count; i++)
+    {
+        if (responseTimes[i] > responseTimes.GetRange(0, i).Average())
+        {
+            result++;
+        }
+    }
+
+    return result;
+    
+}
